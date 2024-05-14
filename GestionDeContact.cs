8ae9066f85cb7e175749525d4696c25f08165b46
Dorian Belkacem <belkacem.dorian@esh64.fr>
2024-05-14 10:21:53 +0200

    Création de la classe GestionDeContact et mise en place du CRUD

---
+++ GestionDeContact.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoListeContact
+{
+   public class GestionDeContact : Contact
+    {
+        List<Contact> contacts = new List<Contact>();
+        private int id = 1;
+        public void AddContact(string Name, string LastName, string PhoneNumber, string Email)
+        {
+            Contact newContact = new Contact()
+            {
+                Id = id++
+            };
+
+            Console.WriteLine("Entrez le Nom:");
+            newContact.Name = Console.ReadLine();
+
+            Console.WriteLine("Entrez le Prénom:");
+            newContact.LastName = Console.ReadLine();
+
+            Console.WriteLine("Entrez l'Email");
+            newContact.Email = Console.ReadLine();
+
+            Console.WriteLine("Téléphone");
+            newContact.PhoneNumber = Console.ReadLine();
+        }
+
+        public void UpdateContact(int id, string name, string LastName, string PhoneNumber, string Email) 
+        {
+
+        }
+
+        
+    }
+}
